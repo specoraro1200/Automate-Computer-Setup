11440f7a062783c60586e811fc6823380ea8d878
specoraro1200 <69777511+specoraro1200@users.noreply.github.com>
2022-01-09 12:49:46 -0600

    download.sh

---
+++ filename.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install dockutil
+brew install google-chrome
+brew install firefox
+brew install snip
+brew install adobe-acrobat-reader
+brew install zoom
+brew install --cask onedrive
+brew install microsoft-powerpoint
+brew install microsoft-word 
+brew install microsoft-excel
+
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/System/Library/Desktop Pictures/Solid Colors/Black.png"' 
+osascript -e 'tell app "System Events" to set current screen saver to screen saver "FloatingMessage"'
+
+
+dockutil --remove "News"
+dockutil --remove "TV"
+dockutil --remove "Podcasts"
+dockutil --remove "Music"
+dockutil --remove "Photos"
+dockutil --remove "App Store"
+dockutil --add /Applications/FireFox.app --position 1
+dockutil --add /Applications/Adobe\ Acrobat\ Reader\ DC.app
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/Microsoft\ Word.app
+dockutil --add /Applications/Adobe\ Acrobat\ Reader\ DC.app
+dockutil --add /Applications/Microsoft\ Excel.app
+dockutil --add /Applications/Microsoft\ PowerPoint.app
+dockutil --add /Applications/OneDrive.app
